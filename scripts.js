80ec7be4052a3870e37d21081bcbaa740a4993ef
Nandin <Machadofernando986@gmail.com>
2026-07-16 16:04:51 -0300

    Em andamento. Os botões sao acionados e o Clean também. mas ainda falta arrumar a lógica.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function  digitar (value) {
+
+    let resultado = document.getElementById("resultado");
+
+
+resultado.textContent += value
+
+
+}
+
+function limpar () {
+   let  clean = document.getElementById("resultado");
+
+   clean.textContent = 0
+}
+
